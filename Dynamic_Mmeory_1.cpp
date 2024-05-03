b5f13a49bb6c113ae15320a9c126dea0489b0c51
Ronak786 <sunronak@yahoo.co.in>
2024-05-03 22:19:20 +0530

    Dynamic Mmemory Example 1

---
+++ Dynamic_Mmeory_1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <memory>
+
+class dynamic_memory_1
+{
+
+
+
+
+};
+
+
+int main()
+{
+
+
+int *a;
+
+a = new int(5);
+
+std::cout << "Value of a = " << *a << std::endl;
+
+}
