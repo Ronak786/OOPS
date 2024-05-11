8835b9b4c08953b0b791ff16199663e28b0995f9
Ronak786 <sunronak@yahoo.co.in>
2024-05-11 19:41:37 +0530

    Constant Function

---
+++ Constant_Function.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+
+
+class Constant_Class
+{
+
+    private: 
+        int x;
+        int y;
+        const int cons_variable;
+    public:
+        void setdata();
+        void getdata();    
+        
+        /**
+         *  Constant member initlizer.
+        */
+        Constant_Class():cons_variable(67)
+        {
+             //code here    
+        }
+
+        void constant_function() const;
+        
+
+    
+        
+
+};
+
+
+void Constant_Class::getdata()
+{
+  
+    std::cout<< "Value of x: " << x << std::endl;
+      
+    std::cout<< "Value of y: " << y << std::endl;
+
+    std::cout<< "Value of Constant: " << cons_variable << std::endl;  
+
+}
+
+void Constant_Class::constant_function() const
+{
+    /**
+     * 1. We cant modify any value here
+     * 2. This type of function called access function.
+    */
+    std::cout<< "We are in constant function" << std::endl;
+    
+    //std::cin>>x;
+}
+
+void Constant_Class::setdata()
+{ 
+      std::cout<< "Enter For x: " << std::endl;
+      std::cin >> x;
+      std::cout<< "Enter For y: " << std::endl;
+      std::cin >> y;
+
+
+      //std::cin >> cons_variable;
+}
+
+int main()
+{
+
+     Constant_Class Constant_Obj;
+
+     Constant_Obj.setdata();
+     Constant_Obj.getdata();
+     Constant_Obj.constant_function();
+
+}
