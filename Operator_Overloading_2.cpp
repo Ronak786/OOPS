fa6751e1a3e8855b446d4b5b20929bf375480cfa
Ronak786 <sunronak@yahoo.co.in>
2024-05-18 17:32:31 +0530

    Operator Overload +

---
+++ Operator_Overloading_2.cpp
@@ -0,0 +1,126 @@
+#include<iostream>
+
+
+class Op_Overload
+{
+
+    private:
+
+    int a;
+
+    public:
+
+    void get_data();
+    void set_data();
+
+
+   /**
+    * 1. Two types of overloading: 1. Binary 2 Unery
+    * 2. Binary Overload: Needs two objects
+    * 3. Unery Overload : Needs one objects.
+    * 4. In binary overload 1st objects called imlplicite objects and 2nd called explicite object.
+    * 5. (Important) Declaration should inside class and defination should outside class.
+    *    if its a friend function overload defination and declaration can be inside class.
+    * 6. Operator overload defination is same as function defination.
+   */
+    void operator == (Op_Overload &Test);
+    Op_Overload operator +  (Op_Overload &Test);
+
+};
+
+class Op_Overload_2
+{
+
+    private:
+
+    int b;
+
+    public:
+
+    void get_data_2();
+    void set_data_2();
+
+
+};
+
+
+void Op_Overload ::set_data()
+{
+    std::cout << "Enter Data: " << std::endl;
+    std::cin >> a;
+
+}
+
+void Op_Overload ::get_data()
+{
+
+    std::cout << "Value of a: " << a << std::endl;
+    
+}
+
+void Op_Overload::operator==(Op_Overload &Test)
+{
+
+        std::cout << "We are in Operator Ovrload Function succesfully" << std::endl;
+
+        if( this->a == Test.a )
+        {
+            std::cout << "Both objects are equal" << std:: endl;
+        }
+        else
+        {
+            std::cout << "Both objects are not equal" << std:: endl;
+        }
+
+}
+
+Op_Overload Op_Overload::operator+(Op_Overload &Test)
+{
+    std::cout << "We are in + Operator Overload" << std::endl;
+
+    Op_Overload Result_Obj;
+
+    Result_Obj.a = this->a + Test.a;
+
+    return Result_Obj;
+     
+
+}
+
+void Op_Overload_2 ::get_data_2()
+{
+    std::cout << "Enter Data: " << std::endl;
+    std::cin >> b;
+
+}
+
+void Op_Overload_2 ::set_data_2()
+{
+
+    std::cout << "Value of b: " << b << std::endl;
+    
+}
+
+
+int main()
+{
+
+    Op_Overload Op_Overload_Obj_1 , Op_Overload_Obj_2 ,Op_Overload_Obj_3;
+    //Op_Overload_2 Op_Overload_2_Obj;
+    
+    Op_Overload_Obj_1.set_data();
+    Op_Overload_Obj_2.set_data();
+
+    Op_Overload_Obj_1.get_data();
+    Op_Overload_Obj_2.get_data();
+
+
+    //Op_Overload_Obj_1 == Op_Overload_Obj_2;
+    
+    Op_Overload_Obj_3 = Op_Overload_Obj_1 + Op_Overload_Obj_2; 
+
+    Op_Overload_Obj_3.get_data();
+
+    
+
+}
